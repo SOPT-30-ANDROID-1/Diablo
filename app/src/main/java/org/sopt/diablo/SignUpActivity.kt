0c8b8fb1df0cbb608c67019b28fbc6a54939319a
xxeol2 <jhj01177@naver.com>
2022-04-09 16:41:25 +0900

    feat: 로그인/회원가입 기능 구현

---
+++ app/src/main/java/org/sopt/diablo/SignUpActivity.kt
@@ -0,0 +1,39 @@
+package org.sopt.diablo.view
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import org.sopt.diablo.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+    }
+
+    private fun initView() {
+        binding.btnSignup.setOnClickListener {
+            if (checkInput()) {
+                Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
+                val intent = Intent().apply {
+                    putExtra("id", binding.etId.text.toString())
+                    putExtra("pw", binding.etPw.text.toString())
+                }
+                setResult(Activity.RESULT_OK, intent)
+                finish()
+            } else {
+                Toast.makeText(this, "아이디/비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun checkInput(): Boolean {
+        return binding.etName.text.isNotEmpty() && binding.etId.text.isNotEmpty() && binding.etPw.text.isNotEmpty()
+    }
+
+}
